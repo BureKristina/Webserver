53c0220f1e2a5c764afe442aa45bbcfbfffe1cbb
KristinaBure <tina.bure@gmail.com>
2020-11-19 15:05:08 +0200

    dynamic analysis commit

--- src/webserver/WebServerTest.java
+++ src/webserver/WebServerTest.java
@@ -12,17 +12,17 @@ public class WebServerTest {
 
     private static WebServer ws;
     private static final String PATH = "D:\\IntelliJ\\Projects\\VVS Webserver\\HTML Files\\";
-    private static final String startHTML = "<!DOCTYPE html>\r\n";
-    private static final String styleHTML = "<style>\r\n" +
-            "  body { text-align: center; padding: 150px; }\r\n" +
-            "  h1 { font-size: 50px; }\r\n" +
-            "  body { font: 20px Helvetica, sans-serif; color: #333; }\r\n" +
-            "  article { display: block; text-align: left; width: 650px; margin: 0 auto; }\r\n" +
-            "  a { color: #dc8100; text-decoration: none; }\r\n" +
-            "  a:hover { color: #333; text-decoration: none; }\r\n" +
-            "</style>\r\n";
-    private static final String startBody = "<body>\r\n" + "\r\n";
-    private static final String endBody = "\r\n" + "</body>\r\n" + "</html>";
+    private static final String startHTML = "<!DOCTYPE html>\n";
+    private static final String styleHTML = "<style>\n" +
+            "  body { text-align: center; padding: 150px; }\n" +
+            "  h1 { font-size: 50px; }\n" +
+            "  body { font: 20px Helvetica, sans-serif; color: #333; }\n" +
+            "  article { display: block; text-align: left; width: 650px; margin: 0 auto; }\n" +
+            "  a { color: #dc8100; text-decoration: none; }\n" +
+            "  a:hover { color: #333; text-decoration: none; }\n" +
+            "</style>\n";
+    private static final String startBody = "<body>\n" + "\n";
+    private static final String endBody = "\n" + "</body>\n" + "</html>";
 
     @BeforeClass
     public static void setUp() {
@@ -42,45 +42,45 @@ public class WebServerTest {
     @Test
     public void checkReadFromFileForIndexHtml() {
         String result = ws.readFromFile(PATH + "index.html");
-        String expected = startHTML + "<title>Homepage</title>\r\n" + styleHTML +
-                startBody + "<h1>Welcome!</h1>\r\n" +
-                "<p>Try out this <a href=\"link.html\">link</a>!</p>\r\n" +
-                "<p>Or go to <a href=\"https://www.google.com/\">Google</a></p>\r\n" + endBody;
+        String expected = startHTML + "<title>Homepage</title>\n" + styleHTML +
+                startBody + "<h1>Welcome!</h1>\n" +
+                "<p>Try out this <a href=\"link.html\">link</a>!</p>\n" +
+                "<p>Or go to <a href=\"https://www.google.com/\">Google</a></p>\n" + endBody;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void checkReadFromFileFor404Html() {
         String result = ws.readFromFile(PATH + "404.html");
-        String expected = startHTML + "<title>Error 404</title>\r\n" + startBody +
-                "<h1>404 File Not Found :(</h1>\r\n" + endBody;
+        String expected = startHTML + "<title>Error 404</title>\n" + startBody +
+                "<h1>404 File Not Found :(</h1>\n" + endBody;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void checkReadFromFileForLinkHtml() {
         String result = ws.readFromFile(PATH + "link.html");
-        String expected = startHTML + "<title>Homepage</title>\r\n" + styleHTML +
-                startBody + "<h1>Yaaaaaayy!</h1>\r\n" + "<p>This link works</p>\r\n" +
-                "<p>Go <a href=\"index.html\">back</a> to homepage</p>\r\n" + endBody;
+        String expected = startHTML + "<title>Homepage</title>\n" + styleHTML +
+                startBody + "<h1>Yaaaaaayy!</h1>\n" + "<p>This link works</p>\n" +
+                "<p>Go <a href=\"index.html\">back</a> to homepage</p>\n" + endBody;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void checkReadFromFileForMaintenanceHtml() {
         String result = ws.readFromFile(PATH + "maintenance.html");
-        String expected = startHTML + "<title>Site Maintenance</title>\r\n" + styleHTML + startBody +
-                "    <h1>We&rsquo;ll be back soon!</h1>\r\n" + "    <div>\r\n" +
-                "        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. We&rsquo;ll be back online shortly!</p>\r\n" +
-                "    </div>\r\n" + endBody;
+        String expected = startHTML + "<title>Site Maintenance</title>\n" + styleHTML + "\n" + startBody +
+                "    <h1>We&rsquo;ll be back soon!</h1>\n" + "    <div>\n" +
+                "        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. We&rsquo;ll be back online shortly!</p>\n" +
+                "    </div>\n" + endBody;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void checkReadFromFileForStoppedHtml() {
         String result = ws.readFromFile(PATH + "stopped.html");
-        String expected = startHTML + "<title>Server Stopped</title>\r\n" + startBody +
-                "<h1>Connection Timeout</h1>\r\n" + endBody;
+        String expected = startHTML + "<title>Server Stopped</title>\n" + startBody +
+                "<h1>Connection Timeout</h1>\n" + endBody;
         Assert.assertEquals(result, expected);
     }
 
@@ -110,7 +110,7 @@ public class WebServerTest {
     public void checkIfMeintenanceOk() {
         try {
             int result = ws.stopped(new PrintWriter(ws.clientSocket.getOutputStream(),
-                    true), "meintenance.html");
+                    true), "maintenance.html");
             Assert.assertEquals(result, 1);
         } catch (IOException e) {
             e.printStackTrace();
